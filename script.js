138af6079cdf43bb23268e2fccb2b779e7e25205
martijnkunstman <martijn@kunstman.nl>
2022-11-28 21:23:46 +0100

    join

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let sizeWH = 500;
-let cellCount = 25;
-let pointCount = 2000;
+let sizeWH = 400;
+let cellCount = 30;
+let pointCount = 3000;
 let points = [];
 let cells = [];
 
@@ -8,12 +8,12 @@ let frameTimeElement = document.getElementById('frameTime');
 let frameRateElement = document.getElementById('frameRate');
 let nCountElement = document.getElementById('nCount');
 let shCountElement = document.getElementById('shCount');
-let timesFasterElement = document.getElementById('timesFaster'); 
+let timesFasterElement = document.getElementById('timesFaster');
 
 //p5.disableFriendlyErrors = true; // disables FES
 
 function setup() {
-  
+
   //pixelDensity(1);
   createCanvas(sizeWH, sizeWH);
   for (let i = 0; i < pointCount; i++) {
@@ -45,6 +45,7 @@ function setup() {
       } else {
         fill("white");
       }
+      stroke("rgba(0,0,0,0.1)");
       square(
         (a * sizeWH) / cellCount,
         (b * sizeWH) / cellCount,
@@ -52,29 +53,45 @@ function setup() {
       );
     }
   }
-  nCountElement.innerHTML = "n squared operations:"+(pointCount * pointCount);
+  nCountElement.innerHTML = "n squared operations:" + (pointCount * pointCount);
 }
 let currentTime = 0;
 let oldTime = 0;
 function draw() {
-  currentTime = millis()-oldTime;
+  clear();
+  for (let a = 0; a < cells.length; a++) {
+    for (let b = 0; b < cells[a].length; b++) {
+      if (cells[a][b]) {
+        fill("green");
+      } else {
+        fill("white");
+      }
+      stroke("rgba(0,0,0,0.1)");
+      square(
+        (a * sizeWH) / cellCount,
+        (b * sizeWH) / cellCount,
+        sizeWH / cellCount
+      );
+    }
+  }
+  currentTime = millis() - oldTime;
   oldTime = millis();
-  frameTimeElement.innerHTML = "time:"+Math.round(currentTime);
-  frameRateElement.innerHTML = "frameRate:"+Math.round(frameRate());
+  frameTimeElement.innerHTML = "time:" + Math.round(currentTime);
+  frameRateElement.innerHTML = "frameRate:" + Math.round(frameRate());
   for (let i = 0; i < points.length; i++) {
     points[i][2] = 0;
   }
   counter = 0;
   space();
-  shCountElement.innerHTML = "spacial hash operations:"+counter;
-  timesFasterElement.innerHTML = "times faster:" +Math.round((pointCount * pointCount)/counter);
+  shCountElement.innerHTML = "spacial hash operations:" + counter;
+  timesFasterElement.innerHTML = "times faster:" + Math.round((pointCount * pointCount) / counter);
 
   for (let i = 0; i < points.length; i++) {
     if (
-      points[i][0] < mouseX + sizeWH / cellCount*2 &&
-      points[i][0] > mouseX - sizeWH / cellCount*2 &&
-      points[i][1] < mouseY + sizeWH / cellCount*2 &&
-      points[i][1] > mouseY - sizeWH / cellCount*2
+      points[i][0] < mouseX + sizeWH / cellCount * 2 &&
+      points[i][0] > mouseX - sizeWH / cellCount * 2 &&
+      points[i][1] < mouseY + sizeWH / cellCount * 2 &&
+      points[i][1] > mouseY - sizeWH / cellCount * 2
     ) {
       if (points[i][2] != 2) {
         points[i][2] = 1;
@@ -90,10 +107,18 @@ function draw() {
       fill("white");
     }
     square(points[i][0] - 1.5, points[i][1] - 1.5, 3);
-  } 
+  }
+  counterFrames++;
+    if (counterFrames > 100) {
+      counterFrames = 0;
+      //horizontal = !horizontal;
+    }
 }
 let printme = true;
 let counter = 0;
+
+let counterFrames = 0;
+let horizontal = true;
 
 function space() {
   let pointsArray = [];
@@ -147,7 +172,29 @@ function space() {
           }
         }
         //vind point rondom deze behalve zichzelf
+        
         for (d = 0; d < pointsAround.length; d++) {
+          //check distance from other point, move away from that point if too close
+          if (pointsAround[d] != dezeId) {
+            let xdistance = points[dezeId][0] - points[pointsAround[d]][0];
+            let ydistance = points[dezeId][1] - points[pointsAround[d]][1];
+            let distance = Math.sqrt(xdistance * xdistance + ydistance * ydistance);
+            
+            if (distance < (sizeWH / cellCount)) {
+              if (horizontal)
+              {
+              points[dezeId][0] -= ((xdistance) / (sizeWH / cellCount)) * 0.2;
+              }
+              else
+              {
+              points[dezeId][1] -= ((ydistance) / (sizeWH / cellCount)) * 0.2;
+              }
+            }
+            //console.log(distance);
+            //console.clear();
+          }
+
+          //
           if (a == Math.floor((mouseY / sizeWH) * cellCount)) {
             if (b == Math.floor((mouseX / sizeWH) * cellCount)) {
               points[pointsAround[d]][2] = 2;
